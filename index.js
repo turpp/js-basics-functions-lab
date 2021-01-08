f5394cf425a99a61d76da637534960642895a5c5
turpp <white.tripp@icloud.com>
2021-01-08 16:37:36 -0600

    commit tree ff05774c16b27a0e98be1ed61e761c68c3db2475

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+let hq=42
+function distanceFromHqInBlocks(street){
+    if (street > hq){
+    return street-hq
+    } else {
+        return hq-street
+    }
+}
+
+function distanceFromHqInFeet(street) {
+    return distanceFromHqInBlocks(street) * 264
+}
+
+function distanceTravelledInFeet(startStreet, endStreet){
+    if (startStreet < endStreet){
+        let blocks=endStreet-startStreet
+        return blocks*264
+    } else {
+        let blocks= startStreet - endStreet
+        return blocks*264
+    }
+}
+
+function calculatesFarePrice(startStreet, endStreet){
+    let distance = distanceTravelledInFeet(startStreet, endStreet)-400
+    console.log(distance)
+    if(distance < 0){
+        return 0
+    } else if ((distance > 0)&&(distance < 1600)){
+        return distance*.02
+    } else if (distance > 2100){
+        return "cannot travel that far"
+    } else {
+        return 25
+    }
+}
